00a8ce5cc8a9ef06a462099088500b7d87d8b130
Muzammil099 <muzammilkhan5432@outlook.com>
2024-08-07 20:50:54 +0500

    Add AccountRepository

---
+++ src/test/java/com/practice/payment/repositories/AccountRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.practice.payment.repositories;
+
+import com.practice.payment.models.Account;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Test
+    public void testCreateUser(){
+
+        Account account = new Account();
+        account.setUserId(1L);
+        account.setBalance(BigDecimal.valueOf(1000));
+        accountRepository.save(account);
+
+        Optional<Account> foundAccount = accountRepository.findByUserId(1L);
+        assertThat(foundAccount).isPresent();
+        assertThat(foundAccount.get().getBalance()).isEqualByComparingTo(BigDecimal.valueOf(1000));
+
+
+    }
+}
